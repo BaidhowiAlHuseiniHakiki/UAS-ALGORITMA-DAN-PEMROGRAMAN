bc9e36312d9c29ae0d0984cfdadccd3a31a1024e
Baidhowi Al Huseini Hakiki <144520859+BaidhowiAlHuseiniHakiki@users.noreply.github.com>
2024-07-03 11:04:58 +0700

    Update WarungNasi.java

--- WarungNasi.java
+++ WarungNasi.java
@@ -84,3 +84,56 @@ public class WarungNasi {
         scanner.close();
     }
 }
+
+//OUPUT
+========================================
+SELAMAT DATANG DI WARUNG NASI DHOWI!
+========================================
+Masukkan nama Anda: Baidhowi Al Huseini Hakiki
+========================================
+Hallo, Baidhowi Al Huseini Hakiki!
+DAFTAR MENU MAKANAN:
+1. Nasi Goreng Biasa         Rp 15000
+2. Nasi Goreng Ayam          Rp 17000
+3. Nasi Goreng Ati Ampela    Rp 17000
+4. Nasi Goreng Seafood       Rp 18000
+5. Nasi Goreng Bakso Sosis   Rp 19000
+6. Mie Goreng Ayam           Rp 15000
+7. Mie Goreng Bakso Sosis    Rp 15000
+8. Mie Kwetiaw Ayam          Rp 15000
+9. Mie Kwetiaw Bakso Sosis   Rp 15000
+10. Mie Kwetiaw Seafood       Rp 18000
+
+DAFTAR MENU MINUMAN:
+11. Air Mineral               Rp 3000
+12. Es Teh Manis              Rp 5000
+13. Es Jeruk                  Rp 5000
+14. Kopi Hitam                Rp 4000
+15. Teh Tarik                 Rp 4000
+========================================
+Pilih menu / masukkan nomor menu (0 untuk selesai): 3
+Anda memesan Nasi Goreng Ati Ampela
+Pilih menu / masukkan nomor menu (0 untuk selesai): 4
+Anda memesan Nasi Goreng Seafood
+Pilih menu / masukkan nomor menu (0 untuk selesai): 2
+Anda memesan Nasi Goreng Ayam
+Pilih menu / masukkan nomor menu (0 untuk selesai): 12
+Anda memesan Es Teh Manis
+Pilih menu / masukkan nomor menu (0 untuk selesai): 12
+Anda memesan Es Teh Manis
+Pilih menu / masukkan nomor menu (0 untuk selesai): 13
+Anda memesan Es Jeruk
+Pilih menu / masukkan nomor menu (0 untuk selesai): 0
+
+RINGKASAN PESANAN:
+- Nasi Goreng Ati Ampela
+- Nasi Goreng Seafood
+- Nasi Goreng Ayam
+- Es Teh Manis
+- Es Teh Manis
+- Es Jeruk
+----------------------------------------
+TOTAL PEMBELIAN: Rp 67000
+Masukkan jumlah uang: 100000
+TERIMA KASIH Sudah Membeli Makanan Di WARUNG NASI DHOWI!
+Ini kembalian Anda: Rp 33000
